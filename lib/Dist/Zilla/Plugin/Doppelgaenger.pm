c304ba01094375ecbed311f3fdaedc1f793cfddb
David Golden <dagolden@cpan.org>
2010-12-17 08:38:34 -0500

    check in lots of hackish work

--- lib/Dist/Zilla/Plugin/Doppelgaenger.pm
+++ lib/Dist/Zilla/Plugin/Doppelgaenger.pm
@@ -9,7 +9,11 @@ use MooseX::Types::Path::Class qw(Dir File);
 use MooseX::Types::URI qw(Uri);
 use MooseX::Types::Perl qw(ModuleName);
 with 'Dist::Zilla::Role::FileGatherer';
+with 'Dist::Zilla::Role::TextTemplate';
+with 'Dist::Zilla::Role::FileMunger'; # for Changes
+with 'Dist::Zilla::Role::AfterRelease'; # for Changes
 
+use Dist::Zilla::File::InMemory;
 use File::Find::Rule;
 use File::pushd qw/tempd/;
 use Path::Class;
@@ -19,6 +23,10 @@ use HTTP::Tiny;
 use JSON;
 
 use namespace::autoclean;
+
+#--------------------------------------------------------------------------#
+# public attributes
+#--------------------------------------------------------------------------#
 
 =attr source_module (REQUIRED)
 
@@ -31,18 +39,28 @@ has source_module => (
   isa   => ModuleName,
   required => 1,
 );
-  
-=attr new_name (REQUIRED)
 
-The new name to use in place of the source name
+=attr new_name
+
+The new name to use in place of the source name.  Defaults to
+the converted form of the distribution name.
 
 =cut
 
 has new_name => (
   is    => 'ro',
   isa   => ModuleName,
+  lazy  => 1,
   required => 1,
+  builder => '_build_new_name',
 );
+
+sub _build_new_name {
+  my ($self) = shift;
+  my $name = $self->zilla->name;
+  $name =~ s{-}{::}g;
+  return $name;
+}
 
 =attr cpan_mirror
 
@@ -57,14 +75,96 @@ has cpan_mirror => (
   coerce => 1,
   default => 'http://cpan.dagolden.com/'
 );
-  
+
+=attr strip_pod
+
+Boolean for whether Pod should be stripped when copying from source.
+Default is false.
+
+=cut
+
+has strip_pod => (
+  is    => 'ro',
+  isa   => 'Bool',
+  default => 0,
+);
+
+=attr changes_file
+
+Name of change log file.  Defaults to 'Changes'.
+
+=cut
+
+has changes_file => (
+  is    => 'ro',
+  isa   => 'Str',
+  default => 'Changes',
+);
+
+=attr update_changes_file
+
+Boolean for whether change log should be updated with the
+source distribution when built. Default is true.
+
+=cut
+
+has update_changes_file => (
+  is    => 'ro',
+  isa   => 'Bool',
+  default => 1,
+);
+
+#--------------------------------------------------------------------------#
+# private
+#--------------------------------------------------------------------------#
+
 has _cpanidx => (
   is    => 'ro',
-  isa   => Uri,
-  coerce => 1,
+  isa   => 'Str',
   default => 'http://cpanidx.org/',
 );
 
+has _distfile => (
+  is    => 'ro',
+  isa   => 'Str',
+  lazy  => 1,
+  builder => '_build_distfile',
+);
+
+sub _build_distfile {
+  my ($self) = @_;
+  my $mod = $self->source_module;
+  my $uri = $self->_cpanidx . join("/", qw/cpanidx json mod/ ,$mod);
+  my $response = HTTP::Tiny->new->get("$uri",
+    {
+      headers => { accept => '*' },
+    }
+  );
+  die "Could not find $mod via $uri: $response->{content}\n"
+    unless $response->{success};
+
+  my $meta = decode_json($response->{content})->[0];
+
+  return $meta->{dist_file};
+}
+
+has _short_distfile => (
+  is    => 'ro',
+  isa   => 'Str',
+  lazy  => 1,
+  builder => '_build_short_distfile',
+);
+
+sub _build_short_distfile {
+  my ($self) = @_;
+  my $distfile = $self->_distfile;
+  $distfile =~ s{./../}{};
+  return $distfile;
+}
+
+#--------------------------------------------------------------------------#
+# methods
+#--------------------------------------------------------------------------#
 
 sub gather_files {
   my ($self) = @_;
@@ -72,6 +172,7 @@ sub gather_files {
   my $distfile = $self->_distfile
     or die "Could not find distfile for " . $self->source_module . "\n";
 
+  $self->log([ 'Cloning files from %s', $self->_short_distfile]);
   my $wd = tempd;
   my $tarball = $self->_download($distfile);
   my $ae = Archive::Extract->new( archive => $tarball );
@@ -82,19 +183,34 @@ sub gather_files {
   die "Couldn't find untarred folder for $tarball\n"
   unless $extracted;
 
-  FILE: for my $filename ( $ae->files ) {
+  FILE: for my $filename ( grep { -f } @{$ae->files} ) {
     my $file = file($filename)->relative($extracted);
     next FILE if $file->basename =~ qr/^\./;
     next FILE if grep { /^\.[^.]/ } $file->dir->dir_list;
-    my $dz_file = $self->_file_from_filename($filename);
-    (my $newname = $dz_file->name) =~ s{\A\Q$extracted\E[\\/]}{}g;
-    $newname = Path::Class::dir($newname)->as_foreign('Unix')->stringify;
-
-    $dz_file->name($newname);
+    next FILE unless $file =~ /^(?:lib|t|bin)\//;
+    $self->log_debug([ 'selected %s', $filename ]);
+    my $dz_file = $self->_file_from_filename($filename, $file);
+    $self->_munge_filename($dz_file);
     $self->_munge_file($dz_file);
+    $self->_strip_pod($dz_file);
     $self->add_file($dz_file);
   }
 
+  return;
+}
+
+sub _munge_filename {
+  my ($self, $file) = @_;
+
+  my $old_name = $self->source_module;
+  my $new_name = $self->new_name;
+  s{::}{/}g for $new_name, $old_name;
+
+  (my $new_filename = $file->name) =~ s{$old_name}{$new_name};
+  if ( $new_filename ne $file->name ) {
+    $self->log_debug([ 'renaming %s to %s', $file->name, $new_filename]);
+    $file->name($new_filename);
+  }
   return;
 }
 
@@ -103,41 +219,88 @@ sub _munge_file {
 
   my $old_name = $self->source_module;
   my $new_name = $self->new_name;
-  
+
+  $self->log_debug([ 'updating contents of %s', $file->name ]);
+
   my $content = $file->content;
-  $content =~ s{$old_name}{$self->new_name}g;
+  $content =~ s{$old_name}{$new_name}g;
   $file->content($content);
+}
+
+sub _strip_pod {
+  my ($self, $file) = @_;
+  return unless $self->strip_pod;
+  return unless $file->name =~ m{\.p(?:m|od)\z};
+  $self->log_debug([ 'stripping pod from %s', $file->name ]);
+  my $p = Pod::Strip->new;
+  my $podless;
+  $p->output_string(\$podless);
+  $p->parse_string_document($file->content);
+  $file->content($podless);
+}
+
+sub munge_file {
+  my ($self, $file) = @_;
+  $self->_munge_changes($file) if $file->name eq $self->changes_file;
+}
+
+sub _munge_changes {
+  my ($self, $file) = @_;
+  return unless $self->update_changes_file;
+
+  my $content = $file->content;
+  my $distfile = $self->_short_distfile;
+  my $delim  = $self->delim;
+
+  $content =~ s{ (\Q$delim->[0]\E \s* \$NEXT \s* \Q$delim->[1]\E) }
+               {$1\n\n  - Generated from $distfile\n\n}xs;
+
+  $file->content($content);
+}
+
+sub after_release {
+  my ($self) = @_;
+  return unless $self->update_changes_file && -e $self->changes_file;
+
+  my $file = Dist::Zilla::File::OnDisk->new({
+    name =>  $self->changes_file,
+  });
+  $self->_munge_changes($file);
+  my $content = $file->content;
+
+  my $filename = $self->changes_file;
+
+  $self->log([ 'updating contents of %s on disk', $filename ]);
+
+  # and finally rewrite the changelog on disk
+  open my $out_fh, '>', $filename
+    or Carp::croak("can't open $filename for writing: $!");
+
+  # Win32.
+  binmode $out_fh, ':raw';
+  print $out_fh $content or Carp::croak("error writing to $filename: $!");
+  close $out_fh or Carp::croak("error closing $filename: $!");
 }
 
 sub _download {
   my ($self, $distfile) = @_;
   (my $tarball = $distfile) =~ s{\A.*/([^/]+)\z}{$1};
-  my $uri = file($self->cpan_mirror, qw/authors id/, $distfile); 
+  my $uri = $self->cpan_mirror . join("/", qw/authors id/, $distfile);
   my $response = HTTP::Tiny->new->mirror("$uri", $tarball);
   die "Could not download $uri\n"
     unless $response->{success};
   return $tarball;
 }
 
-sub _distfile {
-  my ($self) = @_;
-  my $mod = $self->source_module;
-  my $uri = file($self->_cpanidx,qw/cpanidx mod/,$mod);
-  my $response = HTTP::Tiny->new->get("$uri");
-  die "Could not find $mod via $uri\n"
-    unless $response->{success};
-
-  my $meta = eval { decode_json($response->{content}) };
-  return $meta->{distfile};
-}
-
 sub _file_from_filename {
-  my ($self, $filename) = @_;
+  my ($self, $filename, $rel_name) = @_;
 
-  return Dist::Zilla::File::OnDisk->new({
-    name => $filename,
+  my $file = Dist::Zilla::File::InMemory->new({
+    name => "$rel_name",
     mode => (stat $filename)[2] & 0755, # kill world-writeability
+    content => do { local (@ARGV,$/)="$filename"; <> },
   });
+  return $file;
 }
 
 __PACKAGE__->meta->make_immutable;

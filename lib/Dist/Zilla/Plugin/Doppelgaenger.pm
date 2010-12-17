041e92cc8e76a77ef6be0dd35428dfc232c6df05
David Golden <dagolden@cpan.org>
2010-12-17 12:28:03 -0500

    ensure strip_version puts back a trailing newline

--- lib/Dist/Zilla/Plugin/Doppelgaenger.pm
+++ lib/Dist/Zilla/Plugin/Doppelgaenger.pm
@@ -254,7 +254,7 @@ sub _strip_version {
     $file->name =~ m{\.pm\z} or $file->content =~ /\A#!.*?perl/;
   $self->log_debug([ 'stripping VERSION from %s', $file->name ]);
   my @lines = grep { $_ !~ $version_re } split "\n", $file->content;
-  $file->content( join("\n",@lines) );
+  $file->content( join("\n",@lines) . "\n" );
 }
 
 sub _strip_pod {

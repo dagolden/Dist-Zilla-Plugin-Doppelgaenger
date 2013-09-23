d0225735f267ec51b95efd7613dc67a05982c054
David Golden <dagolden@cpan.org>
2013-09-23 08:14:58 -0400

    tidy code

--- lib/Dist/Zilla/Plugin/Doppelgaenger.pm
+++ lib/Dist/Zilla/Plugin/Doppelgaenger.pm
@@ -1,5 +1,6 @@
 use strict;
 use warnings;
+
 package Dist::Zilla::Plugin::Doppelgaenger;
 # ABSTRACT: Creates an evil twin of a CPAN distribution
 # VERSION
@@ -11,7 +12,7 @@ use MooseX::Types::URI qw(Uri);
 use MooseX::Types::Perl qw(ModuleName);
 with 'Dist::Zilla::Role::FileGatherer';
 with 'Dist::Zilla::Role::TextTemplate';
-with 'Dist::Zilla::Role::FileMunger'; # for Changes
+with 'Dist::Zilla::Role::FileMunger';   # for Changes
 with 'Dist::Zilla::Role::AfterRelease'; # for Changes
 
 use Dist::Zilla::File::InMemory;
@@ -36,9 +37,9 @@ The name of a CPAN module to imitate.  E.g. Foo::Bar
 =cut
 
 has source_module => (
-  is    => 'ro',
-  isa   => ModuleName,
-  required => 1,
+    is       => 'ro',
+    isa      => ModuleName,
+    required => 1,
 );
 
 =attr new_name
@@ -49,18 +50,18 @@ the converted form of the distribution name.
 =cut
 
 has new_name => (
-  is    => 'ro',
-  isa   => ModuleName,
-  lazy  => 1,
-  required => 1,
-  builder => '_build_new_name',
+    is       => 'ro',
+    isa      => ModuleName,
+    lazy     => 1,
+    required => 1,
+    builder  => '_build_new_name',
 );
 
 sub _build_new_name {
-  my ($self) = shift;
-  my $name = $self->zilla->name;
-  $name =~ s{-}{::}g;
-  return $name;
+    my ($self) = shift;
+    my $name = $self->zilla->name;
+    $name =~ s{-}{::}g;
+    return $name;
 }
 
 =attr cpan_mirror
@@ -71,10 +72,10 @@ Defaults to C<http://www.cpan.org/>
 =cut
 
 has cpan_mirror => (
-  is    => 'ro',
-  isa   => Uri,
-  coerce => 1,
-  default => 'http://www.cpan.org/'
+    is      => 'ro',
+    isa     => Uri,
+    coerce  => 1,
+    default => 'http://www.cpan.org/'
 );
 
 =attr strip_version
@@ -87,9 +88,9 @@ with caution.  Default is false.
 =cut
 
 has strip_version => (
-  is    => 'ro',
-  isa   => 'Bool',
-  default => 0,
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
 );
 
 =attr strip_pod
@@ -101,9 +102,9 @@ Default is false.
 =cut
 
 has strip_pod => (
-  is    => 'ro',
-  isa   => 'Bool',
-  default => 0,
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
 );
 
 =attr changes_file
@@ -113,9 +114,9 @@ Name of change log file.  Defaults to 'Changes'.
 =cut
 
 has changes_file => (
-  is    => 'ro',
-  isa   => 'Str',
-  default => 'Changes',
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'Changes',
 );
 
 =attr update_changes_file
@@ -126,9 +127,9 @@ source distribution when built. Default is true.
 =cut
 
 has update_changes_file => (
-  is    => 'ro',
-  isa   => 'Bool',
-  default => 1,
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 1,
 );
 
 #--------------------------------------------------------------------------#
@@ -136,47 +137,43 @@ has update_changes_file => (
 #--------------------------------------------------------------------------#
 
 has _cpanidx => (
-  is    => 'ro',
-  isa   => 'Str',
-  default => 'http://cpanidx.org/',
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'http://cpanidx.org/',
 );
 
 has _distfile => (
-  is    => 'ro',
-  isa   => 'Str',
-  lazy  => 1,
-  builder => '_build_distfile',
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_distfile',
 );
 
 sub _build_distfile {
-  my ($self) = @_;
-  my $mod = $self->source_module;
-  my $uri = $self->_cpanidx . join("/", qw/cpanidx json mod/ ,$mod);
-  my $response = HTTP::Tiny->new->get("$uri",
-    {
-      headers => { accept => '*' },
-    }
-  );
-  die "Could not find $mod via $uri: $response->{content}\n"
-    unless $response->{success};
+    my ($self) = @_;
+    my $mod = $self->source_module;
+    my $uri = $self->_cpanidx . join( "/", qw/cpanidx json mod/, $mod );
+    my $response = HTTP::Tiny->new->get( "$uri", { headers => { accept => '*' }, } );
+    die "Could not find $mod via $uri: $response->{content}\n"
+      unless $response->{success};
 
-  my $meta = decode_json($response->{content})->[0];
+    my $meta = decode_json( $response->{content} )->[0];
 
-  return $meta->{dist_file};
+    return $meta->{dist_file};
 }
 
 has _short_distfile => (
-  is    => 'ro',
-  isa   => 'Str',
-  lazy  => 1,
-  builder => '_build_short_distfile',
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_short_distfile',
 );
 
 sub _build_short_distfile {
-  my ($self) = @_;
-  my $distfile = $self->_distfile;
-  $distfile =~ s{./../}{};
-  return $distfile;
+    my ($self) = @_;
+    my $distfile = $self->_distfile;
+    $distfile =~ s{./../}{};
+    return $distfile;
 }
 
 #--------------------------------------------------------------------------#
@@ -184,154 +181,156 @@ sub _build_short_distfile {
 #--------------------------------------------------------------------------#
 
 sub gather_files {
-  my ($self) = @_;
+    my ($self) = @_;
 
-  my $distfile = $self->_distfile
-    or die "Could not find distfile for " . $self->source_module . "\n";
+    my $distfile = $self->_distfile
+      or die "Could not find distfile for " . $self->source_module . "\n";
 
-  $self->log([ 'Cloning files from %s', $self->_short_distfile]);
-  my $wd = tempd;
-  my $tarball = $self->_download($distfile);
-  my $ae = Archive::Extract->new( archive => $tarball );
-  $ae->extract
-    or die "Couldn't unpack $tarball: " . $ae->error;
+    $self->log( [ 'Cloning files from %s', $self->_short_distfile ] );
+    my $wd      = tempd;
+    my $tarball = $self->_download($distfile);
+    my $ae      = Archive::Extract->new( archive => $tarball );
+    $ae->extract
+      or die "Couldn't unpack $tarball: " . $ae->error;
 
-  my ($extracted) = grep { -d } dir($wd)->children;
-  die "Couldn't find untarred folder for $tarball\n"
-  unless $extracted;
+    my ($extracted) = grep { -d } dir($wd)->children;
+    die "Couldn't find untarred folder for $tarball\n"
+      unless $extracted;
 
-  FILE: for my $filename ( grep { -f } @{$ae->files} ) {
-    my $file = file($filename)->relative($extracted);
-    next FILE if $file->basename =~ qr/^\./;
-    next FILE if grep { /^\.[^.]/ } $file->dir->dir_list;
-    next FILE unless $file =~ /^(?:lib|t|bin)\//;
-    $self->log_debug([ 'selected %s', $filename ]);
-    my $dz_file = $self->_file_from_filename($filename, $file);
-    $self->_munge_filename($dz_file);
-    $self->_munge_file($dz_file);
-    $self->_strip_version($dz_file);
-    $self->_strip_pod($dz_file);
-    $self->add_file($dz_file);
-  }
+    FILE: for my $filename ( grep { -f } @{ $ae->files } ) {
+        my $file = file($filename)->relative($extracted);
+        next FILE if $file->basename =~ qr/^\./;
+        next FILE if grep { /^\.[^.]/ } $file->dir->dir_list;
+        next FILE unless $file =~ /^(?:lib|t|bin)\//;
+        $self->log_debug( [ 'selected %s', $filename ] );
+        my $dz_file = $self->_file_from_filename( $filename, $file );
+        $self->_munge_filename($dz_file);
+        $self->_munge_file($dz_file);
+        $self->_strip_version($dz_file);
+        $self->_strip_pod($dz_file);
+        $self->add_file($dz_file);
+    }
 
-  return;
+    return;
 }
 
 sub _munge_filename {
-  my ($self, $file) = @_;
+    my ( $self, $file ) = @_;
 
-  my $old_name = $self->source_module;
-  my $new_name = $self->new_name;
-  s{::}{/}g for $new_name, $old_name;
+    my $old_name = $self->source_module;
+    my $new_name = $self->new_name;
+    s{::}{/}g for $new_name, $old_name;
 
-  (my $new_filename = $file->name) =~ s{$old_name}{$new_name};
-  if ( $new_filename ne $file->name ) {
-    $self->log_debug([ 'renaming %s to %s', $file->name, $new_filename]);
-    $file->name($new_filename);
-  }
-  return;
+    ( my $new_filename = $file->name ) =~ s{$old_name}{$new_name};
+    if ( $new_filename ne $file->name ) {
+        $self->log_debug( [ 'renaming %s to %s', $file->name, $new_filename ] );
+        $file->name($new_filename);
+    }
+    return;
 }
 
 sub _munge_file {
-  my ($self, $file) = @_;
+    my ( $self, $file ) = @_;
 
-  my $old_name = $self->source_module;
-  my $new_name = $self->new_name;
+    my $old_name = $self->source_module;
+    my $new_name = $self->new_name;
 
-  $self->log_debug([ 'updating contents of %s', $file->name ]);
+    $self->log_debug( [ 'updating contents of %s', $file->name ] );
 
-  my $content = $file->content;
-  $content =~ s{$old_name}{$new_name}g;
-  $file->content($content);
+    my $content = $file->content;
+    $content =~ s{$old_name}{$new_name}g;
+    $file->content($content);
 }
 
 # match a line that appears to assign to a $VERSION variable
 # it's a bit more liberal on package names that Perl allows, oh, well
 my $version_re = qr/\$(?:(?i)[a-z0-9]+::){0,}VERSION\s*=\s*/;
+
 sub _strip_version {
-  my ($self, $file) = @_;
-  return unless $self->strip_version;
-  return unless
-    $file->name =~ m{\.pm\z} or $file->content =~ /\A#!.*?perl/;
-  $self->log_debug([ 'stripping VERSION from %s', $file->name ]);
-  my @lines = grep { $_ !~ $version_re } split "\n", $file->content;
-  $file->content( join("\n",@lines) . "\n" );
+    my ( $self, $file ) = @_;
+    return unless $self->strip_version;
+    return
+      unless $file->name =~ m{\.pm\z}
+      or $file->content =~ /\A#!.*?perl/;
+    $self->log_debug( [ 'stripping VERSION from %s', $file->name ] );
+    my @lines = grep { $_ !~ $version_re } split "\n", $file->content;
+    $file->content( join( "\n", @lines ) . "\n" );
 }
 
 sub _strip_pod {
-  my ($self, $file) = @_;
-  return unless $self->strip_pod;
-  return unless $file->name =~ m{\.p(?:m|od)\z};
-  $self->log_debug([ 'stripping pod from %s', $file->name ]);
-  my $p = Pod::Strip->new;
-  my $podless;
-  $p->output_string(\$podless);
-  $p->parse_string_document($file->content);
-  $file->content($podless);
+    my ( $self, $file ) = @_;
+    return unless $self->strip_pod;
+    return unless $file->name =~ m{\.p(?:m|od)\z};
+    $self->log_debug( [ 'stripping pod from %s', $file->name ] );
+    my $p = Pod::Strip->new;
+    my $podless;
+    $p->output_string( \$podless );
+    $p->parse_string_document( $file->content );
+    $file->content($podless);
 }
 
 sub munge_file {
-  my ($self, $file) = @_;
-  $self->_munge_changes($file) if $file->name eq $self->changes_file;
+    my ( $self, $file ) = @_;
+    $self->_munge_changes($file) if $file->name eq $self->changes_file;
 }
 
 sub _munge_changes {
-  my ($self, $file) = @_;
-  return unless $self->update_changes_file;
+    my ( $self, $file ) = @_;
+    return unless $self->update_changes_file;
 
-  my $content = $file->content;
-  my $distfile = $self->_short_distfile;
-  my $delim  = $self->delim;
+    my $content  = $file->content;
+    my $distfile = $self->_short_distfile;
+    my $delim    = $self->delim;
 
-  $content =~ s{ (\Q$delim->[0]\E \s* \$NEXT \s* \Q$delim->[1]\E) }
+    $content =~ s{ (\Q$delim->[0]\E \s* \$NEXT \s* \Q$delim->[1]\E) }
                {$1\n\n  - Generated from $distfile}xs;
 
-  $file->content($content);
+    $file->content($content);
 }
 
 sub after_release {
-  my ($self) = @_;
-  return unless $self->update_changes_file && -e $self->changes_file;
+    my ($self) = @_;
+    return unless $self->update_changes_file && -e $self->changes_file;
 
-  my $file = Dist::Zilla::File::OnDisk->new({
-    name =>  $self->changes_file,
-  });
-  $self->_munge_changes($file);
-  my $content = $file->content;
+    my $file = Dist::Zilla::File::OnDisk->new( { name => $self->changes_file, } );
+    $self->_munge_changes($file);
+    my $content = $file->content;
 
-  my $filename = $self->changes_file;
+    my $filename = $self->changes_file;
 
-  $self->log([ 'updating contents of %s on disk', $filename ]);
+    $self->log( [ 'updating contents of %s on disk', $filename ] );
 
-  # and finally rewrite the changelog on disk
-  open my $out_fh, '>', $filename
-    or Carp::croak("can't open $filename for writing: $!");
+    # and finally rewrite the changelog on disk
+    open my $out_fh, '>', $filename
+      or Carp::croak("can't open $filename for writing: $!");
 
-  # Win32.
-  binmode $out_fh, ':raw';
-  print $out_fh $content or Carp::croak("error writing to $filename: $!");
-  close $out_fh or Carp::croak("error closing $filename: $!");
+    # Win32.
+    binmode $out_fh, ':raw';
+    print $out_fh $content or Carp::croak("error writing to $filename: $!");
+    close $out_fh or Carp::croak("error closing $filename: $!");
 }
 
 sub _download {
-  my ($self, $distfile) = @_;
-  (my $tarball = $distfile) =~ s{\A.*/([^/]+)\z}{$1};
-  my $uri = $self->cpan_mirror . join("/", qw/authors id/, $distfile);
-  my $response = HTTP::Tiny->new->mirror("$uri", $tarball);
-  die "Could not download $uri\n"
-    unless $response->{success};
-  return $tarball;
+    my ( $self, $distfile ) = @_;
+    ( my $tarball = $distfile ) =~ s{\A.*/([^/]+)\z}{$1};
+    my $uri = $self->cpan_mirror . join( "/", qw/authors id/, $distfile );
+    my $response = HTTP::Tiny->new->mirror( "$uri", $tarball );
+    die "Could not download $uri\n"
+      unless $response->{success};
+    return $tarball;
 }
 
 sub _file_from_filename {
-  my ($self, $filename, $rel_name) = @_;
+    my ( $self, $filename, $rel_name ) = @_;
 
-  my $file = Dist::Zilla::File::InMemory->new({
-    name => "$rel_name",
-    mode => (stat $filename)[2] & 0755, ## no critic: kill world-writeability
-    content => do { local (@ARGV,$/)="$filename"; <> },
-  });
-  return $file;
+    my $file = Dist::Zilla::File::InMemory->new(
+        {
+            name => "$rel_name",
+            mode => ( stat $filename )[2] & 0755, ## no critic: kill world-writeability
+            content => do { local ( @ARGV, $/ ) = "$filename"; <> },
+        }
+    );
+    return $file;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -368,3 +367,4 @@ the L<Dist::Zilla::Plugin::AppendExternalData> plugin.
 
 =cut
 
+# vim: ts=4 sts=4 sw=4 et:
